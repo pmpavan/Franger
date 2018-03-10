216c8c493372b2d322e59be950d3d34d64027fae
Pavan <pavanpm@freshdesk.com>
2018-03-10 23:22:16 +0530

    contact modules added

--- app/src/main/java/com/frangerapp/franger/app/util/di/module/user/contact/ContactModule.java
+++ app/src/main/java/com/frangerapp/franger/app/util/di/module/user/contact/ContactModule.java
@@ -1,11 +1,23 @@
 package com.frangerapp.franger.app.util.di.module.user.contact;
 
 import android.app.Activity;
+import android.arch.lifecycle.ViewModelProvider;
+import android.content.Context;
 import android.support.annotation.NonNull;
 
+import com.frangerapp.franger.app.util.di.scope.ActivityScope;
+import com.frangerapp.franger.data.common.UserStore;
+import com.frangerapp.franger.domain.profile.interactor.ProfileInteractor;
+import com.frangerapp.franger.domain.user.model.User;
 import com.frangerapp.franger.ui.contact.ContactActivity;
+import com.frangerapp.franger.viewmodel.contact.ContactViewModel;
+import com.frangerapp.franger.viewmodel.invite.InviteUserViewModel;
+
+import org.greenrobot.eventbus.EventBus;
+import org.jetbrains.annotations.NotNull;
 
 import dagger.Module;
+import dagger.Provides;
 
 /**
  * Created by pavanm on 10/03/18.
@@ -16,5 +28,11 @@ public class ContactModule {
 
     public ContactModule(@NonNull ContactActivity activity) {
         this.activity = activity;
+    }
+
+    @Provides
+    @ActivityScope
+    ViewModelProvider.Factory contactActivityViewModel(@NonNull Context context, @NonNull User user, @NonNull UserStore userStore, @NotNull EventBus eventBus, @NotNull ProfileInteractor profileInteractor) {
+        return new ContactViewModel.Factory(context, eventBus, userStore, user, profileInteractor);
     }
 }
